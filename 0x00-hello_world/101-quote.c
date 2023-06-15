e3efae7238f6de7b8918022600b280fa0925c346
Mizfafa <awudief@gmail.com>
2023-06-15 16:54:28 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, msg, 58);
-    return (1);
+char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(STDERR_FILENO, msg, 58);
+return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-    printf("Size of a char: %ld byte(s)\n", sizeof(char));
-    printf("Size of an int: %ld byte(s)\n", sizeof(int));
-    printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-    printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-    printf("Size of a float: %ld byte(s)\n", sizeof(float));
-    printf("Size of a double: %ld byte(s)\n", sizeof(double));
-    return (0);
+printf("Size of a char: %ld byte(s)", sizeof(char));
+printf("Size of an int: %ld byte(s)", sizeof(int));
+printf("Size of a long int: %ld byte(s)", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)", sizeof(long long int));
+printf("Size of a float: %ld byte(s)", sizeof(float));
+printf("Size of a double: %ld byte(s)", sizeof(double));
+return (0);
 }
