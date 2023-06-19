e57ded8be41039f1be761924f7a0a0df345ddccb
Mizfafa <awudief@gmail.com>
2023-06-19 07:09:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<unistd.h>
 /**
- * main- A c program that prints a line
- * Return: 1 (Success)
- */
+* main- A c program that prints a line
+* Return: 1 (Success)
+*/
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
